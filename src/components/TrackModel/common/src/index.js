92549281476b3e9ead2b81ccf75d4f38c52906fa
wudehua <1650024814@qq.com>
2025-07-08 12:57:47 +0800

    feat: 测试部署

--- src/components/TrackModel/common/src/index.js
+++
@@ -1,2 +0,0 @@
-export * from "./map/CesiumWorld.js"
-export * from "./core"
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import '@/assets/css/index.css'
-import App from './App.vue'
-import 'element-plus/dist/index.css'
-
-
-createApp(App).mount('#app')
